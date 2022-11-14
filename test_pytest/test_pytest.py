a054fb35d65ab97f61dca68a7ca8d3c9722a9777
Gordeyev <Aleksey.Gordeyev@METRAN.local>
2022-11-14 16:20:11 +0500

    finalized 2nd hometask testing

--- test_pytest/test_pytest.py
+++ test_pytest/test_pytest.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 sys.path.append(r'c:/Users/AGordeyev/Program Files/Projects/tests_hometask')
-from hometasks import delete_nonunique, dict_values_to_list
+from hometasks import delete_nonunique, dict_values_to_list, find_visits_to_country
 FIXTURE_1 = [
     ([332, 211, 332, 332, 1, 2], [332, 211, 2, 1]),
     ([1, 2, 3, 4, 5, 5, 4, 3, 2, 1], [5, 4, 3, 2, 1])
@@ -15,17 +15,30 @@ FIXTURE_2 = [
     ),    
     ({
         'cityoftroy':['HOMM', 'with', 'cheats', '=', 'lame'],
-        3:['is', 'the', 'best']},
-        ['HOMM', 'with', 'cheats', '=', 'lame', 'is', 'the', 'best']
+        3:['third', 'is', 'the', 'best']},
+        ['HOMM', 'with', 'cheats', '=', 'lame', 'third', 'is', 'the', 'best']
     )        
 ]
 FIXTURE_3 = [
     ([
-    {'visit1': ['Москва', 'Россия']},
-    {'visit2': ['Дели', 'Франция']},
-    {'visit3': ['Владимир', 'Россия']}
+        {'visit1': ['Москва', 'Россия']},
+        {'visit2': ['Дели', 'Франция']},
+        {'visit3': ['Владимир', 'Россия']}
     ],
-            
+        'Франция',
+    [
+        {'visit2': ['Дели', 'Франция']}
+    ]     
+    ),
+    ([  {'visit8': ['Тула', 'Россия']},
+        {'visit9': ['Курск', 'Россия']},
+        {'visit6': ['Лиссабон', 'Португалия']}
+    ],    
+        'Россия',
+    [
+        {'visit8': ['Тула', 'Россия']},
+        {'visit9': ['Курск', 'Россия']}
+    ]
     )
 ]
 class TestFunc():
@@ -39,6 +52,7 @@ class TestFunc():
     def test_dict_to_list(self, _dict, etalon):
         result = dict_values_to_list(_dict)
         assert result == etalon
-    @pytest.mark.parametrize()    
-    def test_find_visits_to_country():
-        
+    @pytest.mark.parametrize('geo_logs, country, etalon', FIXTURE_3)    
+    def test_find_visits_to_country(self, geo_logs, country, etalon):
+        result = find_visits_to_country(country, geo_logs)
+        assert result == etalon
