53638ee3ae15cabd15844f73954c1b6cb72bde0e
Gordeyev <Aleksey.Gordeyev@METRAN.local>
2022-11-14 15:26:57 +0500

    Added 2 hometasks, added 3 more tests

--- hometasks.py
+++ hometasks.py
@@ -1,12 +1,31 @@
-ids = {'user1': [213, 213, 213, 15, 213],
-        'user2': [54, 54, 119, 119, 119],
-        'user3': [213, 98, 98, 35]}
+from hometask_data import IDs, GEO_LOGS, QUERIES
 
-result = []
-for user in list(ids.values()):
-    result.extend(user)
+#---------Hometask #1--------------
+def find_visits_to_country(country, geo_logs):
+    result = []
+    for log in geo_logs:
+        a = list(*log.values())
+        if country in list(*log.values()):
+            result.append(log)
+    return result
+#----------------------------------
+#---------Hometask #2--------------
+def dict_values_to_list(id_info):
+    result = []
+    for user in list(id_info.values()):
+        result.extend(user)
+    return result
     
 def delete_nonunique(values:list):
     return sorted(list(set(values)), reverse = True)
-
-print(delete_nonunique(result))
+#----------------------------------
+#---------Hometask #3--------------
+def get_percentage_of_query_by_words(words_count, queries:list):
+    queries_words_count = {}
+    for elem in queries:
+        queries_words_count[len(elem.split())] = queries_words_count.get(len(elem.split()), 0) + 1
+    return queries_words_count[words_count]
+#----------------------------------
+print(delete_nonunique(dict_values_to_list(IDs)))
+# print(find_visits_to_country('Россия', GEO_LOGS))
+# print(get_percentage_of_query_by_words(3, QUERIES))
--- test_pytest/test_pytest.py
+++ test_pytest/test_pytest.py
@@ -1,13 +1,44 @@
 import pytest
 import sys
 sys.path.append(r'c:/Users/AGordeyev/Program Files/Projects/tests_hometask')
-from hometasks import delete_nonunique
-FIXTURE = [
+from hometasks import delete_nonunique, dict_values_to_list
+FIXTURE_1 = [
     ([332, 211, 332, 332, 1, 2], [332, 211, 2, 1]),
     ([1, 2, 3, 4, 5, 5, 4, 3, 2, 1], [5, 4, 3, 2, 1])
 ]
+FIXTURE_2 = [ 
+    ({
+        1:['abc', 'dce', 'whatever'],
+        2:[1, 2, 3],
+        3:['end']}, 
+        ['abc', 'dce', 'whatever', 1, 2, 3, 'end']
+    ),    
+    ({
+        'cityoftroy':['HOMM', 'with', 'cheats', '=', 'lame'],
+        3:['is', 'the', 'best']},
+        ['HOMM', 'with', 'cheats', '=', 'lame', 'is', 'the', 'best']
+    )        
+]
+FIXTURE_3 = [
+    ([
+    {'visit1': ['Москва', 'Россия']},
+    {'visit2': ['Дели', 'Франция']},
+    {'visit3': ['Владимир', 'Россия']}
+    ],
+            
+    )
+]
 class TestFunc():
-    @pytest.mark.parametrize('_list, etalon',FIXTURE)
-    def test_nonunique_deletion(self, _list, etalon):
+    
+    @pytest.mark.parametrize('_list, etalon',FIXTURE_1)
+    def test_nonunique_deletion_1(self, _list, etalon):
         result = delete_nonunique(_list)
         assert result == etalon
+        
+    @pytest.mark.parametrize('_dict, etalon', FIXTURE_2)
+    def test_dict_to_list(self, _dict, etalon):
+        result = dict_values_to_list(_dict)
+        assert result == etalon
+    @pytest.mark.parametrize()    
+    def test_find_visits_to_country():
+        
